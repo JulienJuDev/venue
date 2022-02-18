3bf9cae3108224adbf00955a3a2bb52bec0df892
julien_dev <julienbourgeois.dev@gmail.com>
2022-02-18 13:50:00 +0100

    Initial commit

---
+++ src/main/kotlin/com/venueapp/venue/VenueApplication.kt
@@ -0,0 +1,11 @@
+package com.venueapp.venue
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VenueApplication
+
+fun main(args: Array<String>) {
+	runApplication<VenueApplication>(*args)
+}
